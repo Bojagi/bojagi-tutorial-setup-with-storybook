8e0c8a055ba3ca94f934cd74c89721692dd64020
Simon Jentsch <simon@bojagi.io>
2021-04-20 17:21:58 +0200

    Remove remaining styled-components references

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,13 @@
 import React from 'react';
-import styled, { createGlobalStyle, css } from 'styled-components';
 import { Button } from './components/Button.jsx';
 import { Slider } from './components/Slider.jsx';
 
-const GlobalStyle = createGlobalStyle(css`
-  html, body {
-    margin: 0;
-    padding: 8px;
-  }
-`);
 
-const CompWrapper = styled.div`
-  margin: 12px;
-`;
+const CompWrapper = ({children}) => <div style={{margin: 12}}>{children}</div>
 
 export function App() {
   return (
     <>
-      <GlobalStyle />
       <CompWrapper>
         <Button>Hello</Button>
       </CompWrapper>
